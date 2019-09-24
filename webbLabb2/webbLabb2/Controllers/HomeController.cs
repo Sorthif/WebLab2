1e1e3d96529cf286395225457d8f26e7db64c706
Nick Grannas <Spec_89@hotmail.com>
2019-09-24 11:21:50 +0200

    Missed a spot...

--- webbLabb2/webbLabb2/Controllers/HomeController.cs
+++ webbLabb2/webbLabb2/Controllers/HomeController.cs
@@ -22,12 +22,6 @@ namespace webbLabb2.Controllers
             return View(await _context.Article.ToListAsync());
         }
 
-        [HttpGet]
-        public IActionResult Admin()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
