18c7ea6d1004ee4de278547b0e3ec33861e590d8
Nick Grannas <Spec_89@hotmail.com>
2019-10-03 15:00:38 +0200

    Worked on the script for searching a database and started working on the layout for the searchbar

---
+++ webbLabb2/webbLabb2/wwwroot/js/Search.js
@@ -0,0 +1,23 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function showHint(str, url) {
+    if (str.length == 0) {
+        document.getElementById("txtHint").innerHTML = "";
+        return;
+    }
+    else {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("txtHint").innerHTML = this.responseText;
+            }
+        };
+
+
+        xmlhttp.open("GET", url + "/Articles/Search/?searchString=" + str, true);
+
+        xmlhttp.send();
+    }
+}
