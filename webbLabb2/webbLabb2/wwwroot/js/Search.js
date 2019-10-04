1eb7cd71e2363dba0f80e508bc838e600012ad1a
Nick Grannas <Spec_89@hotmail.com>
2019-10-04 11:18:27 +0200

    Changed name of the function to a more descriptive name. Tried to fix the CSS for the dropdown unsuccessfully. It is in a "OK" state.

--- webbLabb2/webbLabb2/wwwroot/js/Search.js
+++ webbLabb2/webbLabb2/wwwroot/js/Search.js
@@ -2,16 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-function showHint(str) {
+function searchArticles(str) {
     if (str.length == 0) {
-        document.getElementById("txtHint").innerHTML = "";
+        document.getElementById("searchResults").innerHTML = "";
         return;
     }
     else {
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("txtHint").innerHTML = this.responseText;
+                document.getElementById("searchResults").innerHTML = this.responseText;
             }
         };
 
